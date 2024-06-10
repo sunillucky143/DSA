ca19fdfe5f640458cd4c6c50bb3e9572ec3dcf33
sunillucky143 <58873633+sunillucky143@users.noreply.github.com>
2024-06-10 09:57:37 -0700

    Create binary_search.cpp

---
+++ Searching/binary_search.cpp
@@ -0,0 +1,25 @@
+// Binary Search
+/*This algorithm is used to find the position of target value within a sorted array.
+
+It uses to reduce the time complexity to O(log N)
+*/
+
+//Assume the array is sorted or input only the sorted array.
+
+int binary_search(int arr[], int l, int h, int target){
+  while(low <= high){
+    int mid = low + (high - low)/2;
+    // check if arr[mid] equals to target
+    if(arr[mid] == target){
+      return mid;
+    }
+    if(arr[mid] < traget){
+      low = mid+1;
+    }
+    if(arr[mid] > target){
+      low = mid-1;
+    }
+  }
+  return -1;
+}
+    
